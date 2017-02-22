e31383bef51e79fa8373aef11bd27d107ba52277
niluje <debarge.cedric@gmail.com>
2017-02-23 00:36:23 +0100

    Fix uploader arg number

--- tools/uploader8.py
+++ tools/uploader8.py
@@ -729,7 +729,7 @@ if __name__ == "__main__":
     print("We use Python v%d.%d + PyUSB.%s" %
           (sys.version_info[0], sys.version_info[1],
            "core" if PYUSB_USE_CORE else "legacy"))
-    if len(sys.argv) == 1:
-        sys.exit(main('18f45k50', sys.argv[2]))
+    if len(sys.argv) == 2:
+        sys.exit(main('18f45k50', sys.argv[1]))
     else:
         sys.exit("Usage ex: uploader8.py tools/Blink1459.hex")
